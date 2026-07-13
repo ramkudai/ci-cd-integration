ac017130a9add5e765abc754ed7e0b31588177ac
sucxcz <sencetta@mail.ru>
2026-07-13 10:49:39 +0500

    First name update

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -24,7 +24,7 @@ public class CreateUserTest {
     public void createUser() {
         User user = new User();
         user.setUsername(RandomUtils.getRandomAlphabeticString());
-        user.setFirstName("Rakhmetolla");
+        user.setFirstName("Rakhmet");
         user.setLastName("Kudaibergen");
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
